32f895611e98b28af924be978b2313a757454070
Divyam-12 <patnidivyam@gmail.com>
2023-08-11 01:53:28 +0530

    Home updated (more work has to be done)

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,71 +1,131 @@
 import React from 'react'
-import "./Home.css";
-import img1 from "../Resources/img1.png";
-import img2 from "../Resources/img2.png";
-import animation from "../Resources/animation.png";
+import "./home.css"; 
+// import mini from "./mini.jpg";
+import mini from "./satellite.png";
+import line1 from "./line.png"; 
+import star from "./star.png"; 
+import Animation from "./Animation.js";
+import wallpaper from "./wallpaper.png";
+import bg from "./bg.png";
+import Carousel from 'react-spring-3d-carousel';
+
 
 function Home() {
   return (
     <body>
-      <header>
+      <div className="full">
+      <img src={bg} alt="" className="Topbg1"/>
       
-      <div id="RTP">
-       <p className="RTP">RADIO</p>  
-       <p className="RTP">TELESCOPE</p>  
-       <p className="RTP">PROJECT</p>  
-       <hr className="line" />
-       <div className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</div>
-       <img src={animation} id="animation" alt="" />
-     
+    <div className="bg1">
+    <img src={wallpaper} className="Topbg" alt="" />
+
+    {/* <div className="box">MAI ANIMATION KA DABBA HU</div> */}
+      <div className="animation">
+      <Animation/>
+      </div>
+
+
+
+      <div className="RTP">
+        <div className="RT">RADIO TELESCOPE</div>
+        <div className="RT">PROJECT</div>
+        <div className="line">
+        <img src={star} alt="" />
         </div>
-      </header>
-      
+        
+        <div id="para">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
+        </div>
 
-      <div className="images">
+      </div>   {/* /*RTP ENDS*/}  
+    </div>
 
-        <div className="left">
-        <div className="line1"></div>
-        <div id="tele1">
-          <img src={img1} className="tele1" alt="" />
-          <div className="grad">
-            SMALL RADIO <br />TELESCOPE 
+
+      <div id="telescope">
+        <div className="head">TELESCOPES</div>
+        <img src={line1} alt="" />
+          
+
+        <div id="mini">
+          <div className="miniLeft">
+            <img className="border"src={mini} alt="" />
           </div>
-        </div>
-        <div className="line2"></div>
-        </div>
 
+          <div className="miniRight">
 
+            <div className="top">MINI TELESCOPE</div>
 
+            <div className="mid">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. ioasjdiojasoidjaoijsdoijasoadmopaskdopaskopdkopkapdoakpdokask
+            
+            </div>
 
-        <div className="center">
-          T <br />
-          H <br />
-          E <br /> <br />
-          T <br />
-          E <br />
-          L <br />
-          E <br />
-          S <br />
-          C <br />
-          O <br />
-          P <br />
-          E <br />
-          S <br />
-        </div>
+            <div className="bottom">
+              <button className="read">Read More &gt; </button>
+            </div>
 
-
-        <div className="right">
-        <div className="line3"></div>
-        <div id="tele2">
-          <img src={img2} className="tele2" alt="" />
-          <div className="grad">
-            MINI RADIO <br />TELESCOPE 
           </div>
+        
         </div>
-        <div className="line4"></div>
+
+        <div id="small">
+          <div className="miniLeft">
+            <img className="border"src={mini} alt="" />
+          </div>
+
+          <div className="miniRight">
+
+            <div className="top">SMALL TELESCOPE</div>
+
+            <div className="mid">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+            
+            </div>
+
+            <div className="bottom">
+              <button className="read">Read More &gt; </button>
+            </div>
+
+          </div>
+
+          
+
+        
+
+
+        
+
         </div>
 
 
+        <div id="large">
+          <div className="miniLeft">
+            <img className="border"src={mini} alt="" />
+          </div>
+
+          <div className="miniRight">
+
+            <div className="top">LARGE TELESCOPE</div>
+
+            <div className="mid">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+            
+            </div>
+
+            <div className="bottom">
+              <button className="read">Read More &gt; </button>
+            </div>
+
+          </div>
+
+        </div>
+
+
+      </div>
+        
       </div>
     </body>
   )
